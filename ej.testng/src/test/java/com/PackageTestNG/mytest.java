34012725c758c3b5c9d64492720418675620ee2f
Nikhil boriwale <boriwale.n@gmail.com>
2019-02-11 13:08:49 +0530

    remove space

--- ej.testng/src/test/java/com/PackageTestNG/mytest.java
+++ ej.testng/src/test/java/com/PackageTestNG/mytest.java
@@ -5,29 +5,27 @@ import org.testng.annotations.Test;
 public class mytest {
 
 	// Use of priority and description method
-	
-	@Test(priority =1,description="this test case for login application")
-public void loginApplication() {
-	
-	System.out.println("login ");
-	
-}
-@Test(priority =2,description="this test case for select item")
-	public void Selecteditem() {
-		
-	System.out.println("method Selecteditem  ");
+
+	@Test(priority = 1, description = "this test case for login application")
+	public void loginApplication() {
+		System.out.println("login ");
 
 	}
 
-@Test(priority =3,description="this test case for checkout")
-public void checkout() {
-	System.out.println("method checkout");
+	@Test(priority = 2, description = "this test case for select item")
+	public void Selecteditem() {
+		System.out.println("method Selecteditem  ");
 
-	
-}
-public static void main(String[] args) {
-	
-}
+	}
 
+	@Test(priority = 3, description = "this test case for checkout")
+	public void checkout() {
+		System.out.println("method checkout");
+
+	}
+
+	public static void main(String[] args) {
+
+	}
 
 }
